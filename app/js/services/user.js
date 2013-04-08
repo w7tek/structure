e864f267d1a0381a91c6592e66d53584e1803988
Tommy Knowlton <tommy@w7tek.us>
2013-04-07 22:28:25 -0600

    Remove faulty reference to app.css Point $resource at /r/users for user service

--- app/js/services/user.js
+++ app/js/services/user.js
@@ -1,6 +1,6 @@
 /* user service */
 angular.module('app').factory('User', function($resource){
-  return $resource('user/:id', {id: '@id'}, {
+  return $resource('/r/user/:id', {id: '@id'}, {
     //Angular does a POST by default for create and update
     //this adds an $update method that will do a PUT
     update: {method: 'PUT'}
